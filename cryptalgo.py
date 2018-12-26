8984a29e8ca3c06c50c7dc83a8e2a447c71ab92c
edsabi <38739707+edsabi@users.noreply.github.com>
2018-12-26 11:01:25 -0500

    Update cryptalgo.py

--- cryptalgo.py
+++ cryptalgo.py
@@ -9,33 +9,53 @@ class Cryptoshiz(object):
         pass
 
     def set_encryption_file(self):
-
-        encrypt_this_file = raw_input('Input file path to encrypt\n')
-        #read file to string
+	try:
+            encrypt_this_file = raw_input('Input file path to encrypt\n')
+        except:
+	    encrypt_this_file = input('Input file path to encrypt\n')   
+	#read file to string
         with open(encrypt_this_file, 'r') as plain_text_file:
             self.plaintext_string = plain_text_file.read()
-        self.base_64_encoded_message = base64.b64encode(bytes(self.plaintext_string))
-
+        try:
+	    self.base_64_encoded_message = base64.b64encode(bytes(self.plaintext_string))
+	except:
+	    self.base_64_encoded_message = base64.b64encode(bytes(self.plaintext_string,'utf-8'))
     def set_encrypted_message(self):
-
-        message_to_encode = raw_input('Input Message to encrypt\n')
-        self.base_64_encoded_message = base64.b64encode(bytes(message_to_encode))
-        print(self.base_64_encoded_message)
+	try:
+            message_to_encode = raw_input('Input Message to encrypt\n')
+	except:
+	    message_to_encode = input('Input Message to encrypt\n')
+        try:
+	    self.base_64_encoded_message = base64.b64encode(bytes(message_to_encode))
+        except:
+            self.base_64_encoded_message = base64.b64encode(bytes(message_to_encode,'utf-8'))
+	    
+	print(self.base_64_encoded_message)
 
     def set_encryption_key(self):
         selection = ''
         while selection != 'next':
-            selection = raw_input('[1]Use File to Encrypt\n[2]Use Text to Encrypt\nEnter Selction: ')
+	    try:
+                selection = raw_input('[1]Use File to Encrypt\n[2]Use Text to Encrypt\nEnter Selction: ')
+	    except:
+                selection = input('[1]Use File to Encrypt\n[2]Use Text to Encrypt\nEnter Selction: ')
+		
             if str(selection) == '1':
-
-                encrypt_with_file = raw_input('Input file path to be used as key\n')
+                try:
+                    encrypt_with_file = raw_input('Input file path to be used as key\n')
+		except:
+                    encrypt_with_file = input('Input file path to be used as key\n')
 
                 with open(encrypt_with_file,'rb') as key_file:
                     self.encryption_key = base64.b64encode(bytes(ifile.read())).decode('utf-8').replace('=','')
                 selection = 'next'
             if str(selection) == '2':
-                encrypt_with_string = raw_input('Enter string to be used as key\n')
-                self.encryption_key = (base64.b64encode(bytes(encrypt_with_string)).decode('utf-8')).replace('=','')
+                try:
+		    encrypt_with_string = raw_input('Enter string to be used as key\n')
+                except:
+                    encrypt_with_string = input('Enter string to be used as key\n')
+		    	
+		self.encryption_key = (base64.b64encode(bytes(encrypt_with_string)).decode('utf-8')).replace('=','')
                 selection = 'next'
 
     def do_encryption(self):
@@ -46,7 +66,11 @@ class Cryptoshiz(object):
         bin_list = []
         
         for bchar in self.base_64_encoded_message:
-            bin_list.append(format(ord(bchar), '09b'))
+            try:
+	        bin_list.append(format(ord(bchar), '09b'))
+	    except:
+                bin_list.append("{0:09b}".format(bchar))
+		
         self.bin_string = ''.join(bin_list)
 
         i = 0
@@ -66,13 +90,22 @@ class Cryptoshiz(object):
             i += 1
 
         self.base64_rand_encrypt_string = base64.b64encode(bytes(self.rand_encrypt_string))
-        cipher_path = raw_input('Input File Path to send Ciphertext file\n')
-        success = ''
+        
+	try:
+	    cipher_path = raw_input('Input File Path to send Ciphertext file\n')
+        except:
+            cipher_path = input('Input File Path to send Ciphertext file\n')
+	 
+	success = ''
         while success != '1':
             try:
                 with open(cipher_path,'w') as cipher_file:
                     cipher_file.write(self.base64_rand_encrypt_string.decode('utf-8'))
-                print_cipher = raw_input('Show Cipher?(y/n)\n')
+                try:    
+		    print_cipher = raw_input('Show Cipher?(y/n)\n')
+		except:
+                    print_cipher = input('Show Cipher?(y/n)\n')
+		    
                 success = '1'
             except Exception as e:
                 print(e)
@@ -80,7 +113,11 @@ class Cryptoshiz(object):
             print(self.base64_rand_encrypt_string.decode('utf-8'))
     def input_key(self):
         # open Cipher File
-        open_cipher_file = raw_input('Input File to be Decrypted\n')
+	try:
+            open_cipher_file = raw_input('Input File to be Decrypted\n')
+	except:
+            open_cipher_file = input('Input File to be Decrypted\n')
+
         with open(open_cipher_file,'r') as cipher_file:
             self.cipher_string = cipher_file.read()
 
@@ -88,20 +125,35 @@ class Cryptoshiz(object):
         #open Key File
         selection = ''
         while selection != 'next':
-            selection = raw_input('[1]Use File to Decrypt\n[2]Use Text to decrypt\n')
+            try:
+	        selection = raw_input('[1]Use File to Decrypt\n[2]Use Text to decrypt\n')
+	    except:
+                selection = input('[1]Use File to Decrypt\n[2]Use Text to decrypt\n')
+		
 
             if selection == '1':
-                key_file_input = raw_input('Input Key File\n')
+                try:
+		    key_file_input = raw_input('Input Key File\n')
+		except:
+		    key_file_input = input('Input Key File\n')
                 with open(key_file_input,'rb') as key_file:
                     self.key_string = base64.b64encode(key_file.read()).decode('utf-8').replace('=','')
                 selection = 'next'
 
             if selection == '2':
-                self.key_string = base64.b64encode(bytes(raw_input('Input Key Text\n'))).decode('utf-8').replace('=','')
+                try:
+		    self.key_string = base64.b64encode(bytes(raw_input('Input Key Text\n'))).decode('utf-8').replace('=','')
+		except:
+                    self.key_string = base64.b64encode(bytes(input('Input Key Text\n'),'utf-8')).decode('utf-8').replace('=','')
+		    
                 selection = 'next'
 
     def set_key_string(self):
-        self.key_string = base64.b64encode(bytes(self.key_phrase)).decode('utf-8').replace('=','')
+        try:
+	    self.key_string = base64.b64encode(bytes(self.key_phrase)).decode('utf-8').replace('=','')
+	except:
+            self.key_string = base64.b64encode(bytes(self.key_phrase,'utf-8')).decode('utf-8').replace('=','')
+	    
 
     def do_decryption(self):
         self.ciphered_decoded_base64 = base64.b64decode(self.cipher_string)
@@ -140,11 +192,19 @@ if __name__ == '__main__':
     crypted = Cryptoshiz()
     encrypt_or_decrypt = ''
     while encrypt_or_decrypt != 'next':
-        encrypt_or_decrypt = raw_input('[1]Encrypt\n[2]Decrypt\n')
+        try:
+	    encrypt_or_decrypt = raw_input('[1]Encrypt\n[2]Decrypt\n')
+	except:
+	    encrypt_or_decrypt = input('[1]Encrypt\n[2]Decrypt\n')
+
         if encrypt_or_decrypt == '1':
             selection = ''
             while selection !='next':
-                selection = raw_input('[1]Encrypt File\n[2]Encrypt Text\n')
+                try:
+		    selection = raw_input('[1]Encrypt File\n[2]Encrypt Text\n')
+		except:
+		    selection = input('[1]Encrypt File\n[2]Encrypt Text\n')
+
                 if selection == '1':
                     crypted.set_encryption_file()
                     selection = 'next'
